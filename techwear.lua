de3348ea161b4b9fffcb4483ee0bd585b8100319
kaelixdev-hash <kaelixdev@gmail.com>
2025-12-21 18:48:22 -0300

    Add files via upload

---
+++ techwear.lua
@@ -0,0 +1,19 @@
+    ----------///// SISTEMA-ADMIN /////------------ 
+allowedUsers = {
+    ----------///// TIK-TOK /////------------           
+    
+    
+    
+    ----------///// TEMPORARIOS /////-------------
+    ["Player"]                = true, -- 02/09 27/12
+    
+
+   ----------///// PERMANENTE ACESS /////------------
+    ["HBT_QiOzdb9pNL"]           = true,    
+    
+    ----------///// STAFF /////-------------
+    ["LynixDevOfc"]         = true,
+            
+}
+
+return allowedUsers
